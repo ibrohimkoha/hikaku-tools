8b37b3fd3c426d02a09666e7b9201cab55a371fd
ibrohimkoha <132036128+ibrohimkoha@users.noreply.github.com>
2026-08-30 03:41:53 +0500

    fix: update working b2 credentials and telegram api keys

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
       script: "./b2-uploader-bot",
       env: {
         TELEGRAM_BOT_TOKEN: "8491783785:AAH02Aj6dckq1yk9NYtt1yJ7_TBibi_hrBE",
-        B2_KEY_ID: "005e55e0be912ff0000000001",
-        B2_APP_KEY: "K005/7Vf815HlR2J5E2o3d1uY5o4jE8",
+        B2_KEY_ID: "005562e6b2bafd40000000002",
+        B2_APP_KEY: "K005qO8gFPWarHPO4nEZATyOvJcQ6o4",
         B2_BUCKET_NAME: "iskurama",
         B2_BUCKET_ID: "ce45d5be80bb5e9122ff0010",
         CDN_BASE_URL: "https://cdn.hikaku.uz"
@@ -20,6 +20,11 @@ module.exports = {
       cwd: "./video-compressor-bot",
       script: "python3",
       args: "bot.py",
+      env: {
+        COMPRESSOR_BOT_TOKEN: "8491783785:AAH02Aj6dckq1yk9NYtt1yJ7_TBibi_hrBE",
+        TELEGRAM_API_ID: "33864339",
+        TELEGRAM_API_HASH: "7a12002bdba42778b2068c88bb64072c"
+      },
       autorestart: true,
       max_restarts: 10
     },
