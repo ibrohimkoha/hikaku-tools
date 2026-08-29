7064084565a21003e8fa9a891a21976ec7ec158b
ibrohimkoha <132036128+ibrohimkoha@users.noreply.github.com>
2026-08-30 04:08:07 +0500

    feat: enable dual userbot grabbers for Iskurama and Ayzuken accounts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,12 @@ module.exports = {
     {
       name: "hikaku-b2-uploader",
       cwd: "./b2-uploader-bot",
-      script: "./b2-uploader-bot",
+      script: "python3",
+      args: "bot.py",
       env: {
-        TELEGRAM_BOT_TOKEN: "8491783785:AAH02Aj6dckq1yk9NYtt1yJ7_TBibi_hrBE",
+        TELEGRAM_BOT_TOKEN: "8950963003:AAEMACsSbEz8WXj5TD3n6VNflPhtcoWg3G8",
+        TELEGRAM_API_ID: "33864339",
+        TELEGRAM_API_HASH: "7a12002bdba42778b2068c88bb64072c",
         B2_KEY_ID: "005562e6b2bafd40000000002",
         B2_APP_KEY: "K005qO8gFPWarHPO4nEZATyOvJcQ6o4",
         B2_BUCKET_NAME: "iskurama",
@@ -21,7 +24,7 @@ module.exports = {
       script: "python3",
       args: "bot.py",
       env: {
-        COMPRESSOR_BOT_TOKEN: "8491783785:AAH02Aj6dckq1yk9NYtt1yJ7_TBibi_hrBE",
+        COMPRESSOR_BOT_TOKEN: "8815124197:AAE6zG_54LOh5ja-DxlrGFF8-2cQplEQ5fA",
         TELEGRAM_API_ID: "33864339",
         TELEGRAM_API_HASH: "7a12002bdba42778b2068c88bb64072c"
       },
@@ -29,12 +32,30 @@ module.exports = {
       max_restarts: 10
     },
     {
-      name: "hikaku-userbot-grabber",
+      name: "hikaku-userbot-grabber-1",
       cwd: "./telegram-grabber-userbot",
       script: "python3",
       args: "userbot.py",
+      env: {
+        TELEGRAM_API_ID: "33864339",
+        TELEGRAM_API_HASH: "7a12002bdba42778b2068c88bb64072c",
+        TELEGRAM_SESSION: "hikaku_userbot_session"
+      },
       autorestart: true,
-      max_restarts: 5
+      max_restarts: 10
+    },
+    {
+      name: "hikaku-userbot-grabber-2",
+      cwd: "./telegram-grabber-userbot",
+      script: "python3",
+      args: "userbot.py",
+      env: {
+        TELEGRAM_API_ID: "33864339",
+        TELEGRAM_API_HASH: "7a12002bdba42778b2068c88bb64072c",
+        TELEGRAM_SESSION: "userbot_2"
+      },
+      autorestart: true,
+      max_restarts: 10
     }
   ]
 };
